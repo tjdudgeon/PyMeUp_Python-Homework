714deb0088e7ac9ba1315aca0fff5fc0610ee16c
Taylor Dudgeon <taylordudgeon@Lucass-MacBook-Air.local>
2020-10-30 21:59:06 -0500

    git commit

--- PyBank/Script/main.py
+++ PyBank/Script/main.py
@@ -1,7 +1,24 @@
 import os
 import csv
 
+#Establishing file path to get to CSV
 csvpath = os.path.join("..", "Resources", "budget_data.csv")
 
+#Initializing variable
+total_months = 0
+
+
+#Reading csv
 with open(csvpath) as csvfile:
-    budget_data = csv.reader(csvfile, delimiter=",")
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    #Skip header row
+    next(csv_reader, None)
+
+    #Counting total number of months
+    for months in csv_reader:
+        total_months += 1
+
+    print(total_months)
+        
+ 
